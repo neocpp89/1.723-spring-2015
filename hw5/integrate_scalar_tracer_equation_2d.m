c8ec328f66de4f440833c9860fd688715cd41c41
Sachith Dunatunga <neocpp@gmail.com>
2015-04-02 00:33:31 -0400

    Got something that looks like it's working.

--- hw5/integrate_scalar_tracer_equation_2d.m
+++ hw5/integrate_scalar_tracer_equation_2d.m
@@ -1,4 +1,4 @@
-function [ XYm, Cm, ta ] = integrate_scalar_tracer_equation_2d( Nx, Ny, Pe, theta, dt, tout )
+function [ XYm, Cm, ta, p ] = integrate_scalar_tracer_equation_2d( Nx, Ny, var_lnk, Pe, theta, dt, tout )
 %INTEGRATE_SCALAR_TRACER_EQUATION_2D Solves 2D linear scalar transport equation
 %using finite volumes.
 %  Nx - number of grid cells in x
@@ -11,11 +11,78 @@ function [ XYm, Cm, ta ] = integrate_scalar_tracer_equation_2d( Nx, Ny, Pe, thet
 %  Returns Nx x Ny x (# times) matrix xm containing cell point locations,
 %          Nx x Ny x (# times) matrix cm containing cell solutions at different times,
 %          1 x (# times) matrix ta containing actual times data is saved at.
+%          Nx x Ny matrix containing pressure field.
 
-hx = 1.0 / Nx;
-hy = 1.0 / Ny;
-x = linspace(0+hx/2.0, 1-hx/2.0, Nx);
-y = linspace(0+hy/2.0, 1-hy/2.0, Ny);
+Lx = 1;
+Ly = 1;
+hx = Lx / Nx;
+hy = Ly / Ny;
+x = linspace(0+hx/2.0, Lx-hx/2.0, Nx);
+y = linspace(0+hy/2.0, Ly-hy/2.0, Ny);
+[X,Y] = meshgrid(x,y);
+X = X';
+Y = Y';
+
+corr_lenx = 4*hx;
+corr_leny = 4*hy;
+
+[perm,var_lnk_actual]= random_perm(var_lnk,corr_lenx,corr_leny,Nx,Ny,Lx,Ly);
+perm = perm';
+kinv = 1./perm;
+
+lambda_bar_interior_x = (hy / hx) * 2 ./ (kinv(:,2:Ny) + kinv(:,1:Ny-1));
+lambda_bar_x = zeros(Nx,Ny+1);
+lambda_bar_x(:,2:end-1) = lambda_bar_interior_x(:,:);
+lambda_bar_interior_y = (hx / hy) * 2 ./ (kinv(2:Nx,:) + kinv(1:Nx-1,:));
+lambda_bar_y = zeros(Nx+1,Ny);
+lambda_bar_y(2:end-1,:) = lambda_bar_interior_y(:,:);
+
+
+% Set BCs (default is no-flow)
+b = zeros(Nx,Ny);
+b(1) = 1;
+
+lambda_bar_x(end,end) = (hy / hx) * 2 * perm(end,end);
+lambda_bar_y(end,end) = (hx / hy) * 2 * perm(end,end);
+
+% make diagonals
+T_x_left = reshape(lambda_bar_x(:, 1:end-1), [], 1);
+T_x_right = reshape(lambda_bar_x(:, 2:end), [], 1);
+T_y_bottom = reshape(lambda_bar_y(1:end-1, :), [], 1);
+T_y_top = reshape(lambda_bar_y(2:end, :), [], 1);
+
+sumT = T_x_right + T_x_left + T_y_bottom + T_y_top;
+diags = [-T_x_left, -T_y_bottom, sumT, -T_y_top, -T_x_right];
+
+% assemble matrix
+A = spdiags(diags, [Nx;1;0;-1;-Nx], Nx*Ny, Nx*Ny)';
+bv = reshape(b, [], 1);
+
+% solve for pressure
+pv = A \ bv;
+p = reshape(pv, Nx, Ny);
+
+figure; surf(X,Y,log(perm),'edgecolor','none'); view([0, 0, 1]);
+figure; surf(X,Y,p,'edgecolor','none'); view([0, 0, 1]);
+figure; plot(diag(p) - p(end/2,end/2));
+
+ux = lambda_bar_x(:, 2:end-1).*(p(:, 1:end-1) - p(:,2:end));
+uy = lambda_bar_y(2:end-1, :).*(p(1:end-1,:) - p(2:end,:));
+zx = zeros(size(ux, 1), 1);
+zy = zeros(1, size(uy, 2));
+ux = [zx, ux, zx];
+uy = [zy; uy; zy];
+ux(1,1) = -1/2;
+uy(1,1) = -1/2;
+ux(end,end) = 1/2;
+uy(end,end) = 1/2;
+
+scale = 0.1;
+figure; h = surf(X,Y,zeros(size(X)), log(perm),'edgecolor','none'); view([0, 0, 1]);
+axis equal square;
+hold on;
+quiver(X,Y,scale*(ux(:, 1:end-1)+ux(:, 2:end))/2, scale*(uy(1:end-1, :) + uy(2:end, :))/2, 'Autoscale','off', 'color', [0,0,0]);
+hold off;
 
 e = ones(N, 1);
 z = zeros(N, 1);
